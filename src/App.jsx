5ba5208e42f4ee630618da7aed97d0f1e8c52a66
Leandro Avila - L3anAv <leandroav.dev@gmail.com>
2023-12-23 02:36:22 -0300

    Reestructuracion a una sola pagina, sin saltos. Nuevo diseño.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import Desktop from './Desktop'
-import { Routes, Route } from 'react-router-dom'
+import styled from 'styled-components'
 import Inicio from './core_components/index/index'
+
+const Contenedor = styled.main`
+    width:100vw;
+    height:100vh;
+    display:grid;
+    background: rgb(167,211,151);
+    grid-template-columns: 1fr 1fr;
+    background: linear-gradient(180deg, rgba(167,211,151,1) 0%, rgba(85,88,67,1) 100%); 
+`
+
+const Slogan = styled.p`
+  display: flex;
+  color:#F5EEC8;
+  font-size:80px;
+  user-select:none;
+  text-align:right;
+  align-items: center;
+  justify-content: center;
+  font-family:Quattrocento;
+  animation:OpacidadEntrada 0.5s ease;
+`
 
 export default function App() {
   return (
     <>
-    <Routes>
-        <Route path={"/"} element={<Inicio />} />
-        <Route path={"/os"} element={<Desktop />} />
-      </Routes>
+        <Contenedor>
+        <Inicio />
+        <Slogan>Diseño y Desarrollo el sitio web que te ayude a alcanzar tus objetivos de negocio en línea.</Slogan>
+        </Contenedor>
+        {/* <Desktop /> */}
   </>
   )
 
