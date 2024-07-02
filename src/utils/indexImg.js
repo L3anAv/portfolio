09c2f66cb32759df5a81c5052d42ff4abaa21651
Leandro Avila - L3anAv <leandroav.dev@gmail.com>
2024-07-02 14:26:33 -0300

    eliminando imagenes

--- src/utils/indexImg.js
+++ src/utils/indexImg.js
@@ -1,8 +1,5 @@
-import Cholet from '../assets/images/cholet.webp'
-import StarWars from '../assets/images/starWars.webp'
 import AdcWebMaqueta from '../assets/images/webadc.webp'
 import SpaFlixnet from "../assets/images/spaFlixnet.webp"
-import Curuchet from '../assets/images/casa_curutchet.webp'
 import PortafolioV1 from '../assets/images/PortafolioV1.webp'
 import PortafolioV2 from '../assets/images/PortafolioV2.webp'
 import AsciiDoc from '../assets/images/asciidocgenerator.webp'
@@ -10,8 +7,8 @@ import FlixNetV2Mob from '../assets/images/MobileFlixnetV2.webp'
 import FlixNetV2Web from '../assets/images/WebFlixnetV2.webp'
 import MaquetaArq from '../assets/images/MaquetaEstudioArq.webp'
 
-const Imagenes = [Cholet, StarWars, Curuchet, AdcWebMaqueta, PortafolioV1, PortafolioV2, MaquetaArq, AsciiDoc, SpaFlixnet, FlixNetV2Mob, FlixNetV2Web]
-const nombresImagenes = ["Cholet.png", "StarWars.png", "Curuchet.png", "ADC-Maqueta.webp", "Portafolio-V1.jpg", "Portafolio-V2.jpge", "Maqueta-Estudio.jpg", "AsciiDoc.png", "Spa-Flixnet.png", "Flixnet-Mobile.jpg", "FlixNet-Web-V2.jpg"]
+const Imagenes = [ AdcWebMaqueta, PortafolioV1, PortafolioV2, MaquetaArq, AsciiDoc, SpaFlixnet, FlixNetV2Mob, FlixNetV2Web]
+const nombresImagenes = ["ADC-Maqueta.webp", "Portafolio-V1.jpg", "Portafolio-V2.jpge", "Maqueta-Estudio.jpg", "AsciiDoc.png", "Spa-Flixnet.png", "Flixnet-Mobile.jpg", "FlixNet-Web-V2.jpg"]
 
 function darImagenCorrespondiente(opcion){
     return Imagenes[darIndexSegunNombreDeImagen(opcion)]
